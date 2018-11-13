73ba55ce8aa96024c8f8979a5e196c5977c36b1f
Erika <essnow@wpi.edu>
2018-11-13 18:58:30 -0500

    worked on making the progress bar data observable, but running into some issues

--- Assets/Scripts/FillBarTest.cs
+++ Assets/Scripts/FillBarTest.cs
@@ -1,44 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class FillBarTest : MonoBehaviour {
-    public float percentage; //dummy value for now, goes between 0 and 1
-    public Vector3 originalSize;
-    public Vector3 up;
-    public Vector3 down;
-    public Vector3 direction;
+    private Vector3 originalSize;
+    //private Vector3 up;
+    //private Vector3 down;
+    //private Vector3 direction;
     //empty part of progress bar
     [SerializeField] private GameObject emptyContainer;
 
-	// Use this for initialization
-	void Start () {
-        up = new Vector3(0, 0, 0.005f);
-        down = new Vector3(0, 0, -0.005f);
-        direction = up;
+    VariableDict dict;
+
+    // Use this for initialization
+    void Start () {
+        //up = new Vector3(0, 0, 0.005f);
+        //down = new Vector3(0, 0, -0.005f);
+        //direction = up;
 
         originalSize = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z) * 2;
-        percentage = 0.25f;
-	}
+
+        string robotName = transform.parent.gameObject.name; //name of the image target
+        dict = DataModel.Instance.GetRobotDict(robotName);
+        dict.SetValue("percentage", 0.25f);
+        dict.GetObservableValue("percentage").Subscribe(percentage => {
+            if (percentage == null)
+                return;
+
+            transform.localScale = new Vector3(originalSize.x, originalSize.y, originalSize.z * (float)percentage);
+            emptyContainer.transform.localScale = new Vector3(originalSize.x, originalSize.y, originalSize.z * (1 - (float)percentage));
+        });
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        
+        float value = (float)dict.GetValue("percentage");
+
+        if(value < 1) {
+            value += 0.01f;
+        }
+        else {
+            value = 1;
+        }
+
+        dict.SetValue("percentage", value);
+        Debug.Log("value: " + value);
+
+        /*
         if(transform.localScale.z > .4f && direction == up) {
             direction = down;
         }
         else if(transform.localScale.z < 0.02f && direction == down) {
             direction = up;
         }
-
+        
         transform.localScale += direction;
 
         emptyContainer.transform.localScale -= direction;
-        
+        */
 
         /*
         transform.localScale = new Vector3(originalSize.x, originalSize.y, originalSize.z * percentage);
         emptyContainer.transform.localScale = new Vector3(originalSize.x, originalSize.y, originalSize.z * (1 - percentage));
-   */
+        */
     }
 }
