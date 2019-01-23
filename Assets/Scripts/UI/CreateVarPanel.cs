def33d9355ff37e610566da5bd3cae7b213eab5f
Wheeleral <Awheeler2@wpi.edu>
2019-01-22 20:41:00 -0500

    Added pie

--- Assets/Scripts/UI/CreateVarPanel.cs
+++ Assets/Scripts/UI/CreateVarPanel.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class CreateVarPanel : MonoBehaviour
 {
     public Dropdown d1;
-        List<string> m_DropOptions = new List<string> {};
+    List<string> m_DropOptions = new List<string> {};
     public GameObject varPanel;
     public Text t1;
     // Start is called before the first frame update
@@ -19,7 +19,7 @@ public class CreateVarPanel : MonoBehaviour
     {// this.data.Keys.ToArray();
         Robot r1 = DataManager.Instance.GetRobot("RobotTarget1");
         //m_DropOptions=r1.data.Keys.ToArray();
-        varPanel =  GameObject.Find("OptionPanel");
+        //varPanel =  GameObject.Find("OptionPanel");
         d1.ClearOptions();
         d1.AddOptions(m_DropOptions);
         int options = VisualizationManager.Instance.Options;
@@ -29,7 +29,9 @@ public class CreateVarPanel : MonoBehaviour
                 Vector3 post = new Vector3(t1.transform.position.x, (t1.transform.position.y + offset), t1.transform.position.z);
                 Quaternion q = d1.transform.rotation; 
                 Dropdown  d2 = Instantiate(d1, pos, q, varPanel.transform);//, pos, d1.transform.rotation);
+                d2.name = "d2";
                 Text t2 = Instantiate(t1, post, q, varPanel.transform);//, pos, d1.transform.rotation);
+                t2.name = "t2";
                 t2.text = "Variable " + (i+2);
                 offset = offset + offset;
                 dropdowns.Add(d2);
@@ -42,6 +44,9 @@ public class CreateVarPanel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        d1.ClearOptions();
+        d1.AddOptions(m_DropOptions);
         int options = VisualizationManager.Instance.Options;
         if (options > currOptions) {
             for(int i = 0; i < options - 1 ; i++) {
@@ -49,8 +54,9 @@ public class CreateVarPanel : MonoBehaviour
                 Vector3 post = new Vector3(t1.transform.position.x, (t1.transform.position.y + offset), t1.transform.position.z);
                 Quaternion q = d1.transform.rotation; 
                 Dropdown  d2 = Instantiate(d1, pos, q, varPanel.transform);//, pos, d1.transform.rotation);
+                d2.name = "d2";
                 Text t2 = Instantiate(t1, post, q, varPanel.transform);//, pos, d1.transform.rotation);
-                t2.text = "Variable " + (i+2);
+                t2.name = "t2";                t2.text = "Variable " + (i+2);
                 offset = offset +offset;
             }
             
@@ -62,6 +68,7 @@ public class CreateVarPanel : MonoBehaviour
               //  Destroy(x);
             }
         }
+        
 
     }
 
