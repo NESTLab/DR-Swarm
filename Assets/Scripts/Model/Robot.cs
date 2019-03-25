cd979c7c6750ee9d84ff1c5888a523c23b4c14ee
Jerry Brown <jbbrown@wpi.edu>
2019-03-25 16:30:01 -0400

    Add some documentation comments

--- Assets/Scripts/Model/Robot.cs
+++ Assets/Scripts/Model/Robot.cs
@@ -3,18 +3,35 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 
+/// <summary>
+/// 
+/// </summary>
 public class Robot
 {
     private string _name;
+    /// <summary>
+    /// Name of the robot, readonly.
+    /// </summary>
     public string name
     {
         get { return _name;  }
     }
 
+    /// <summary>
+    /// The color associated with the robot
+    /// </summary>
     public Color color;
 
+    /// <summary>
+    /// A VariableDict which holds the observable data for robot
+    /// </summary>
+    /// <seealso cref="VariableDict"/>
     private VariableDict data;
 
+    /// <summary>
+    /// The constructor for Robot
+    /// </summary>
+    /// <param name="name">The name of the robot</param>
     public Robot(string name)
     {
         this._name = name;
@@ -22,21 +39,33 @@ public class Robot
         this.data = new VariableDict();
     }
 
+    /// <summary>
+    /// Calls <see cref="VariableDict.GetValue{T}(string)"/>
+    /// </summary>
     public T GetVariable<T>(string name)
     {
         return data.GetValue<T>(name);
     }
 
+    /// <summary>
+    /// Calls <see cref="VariableDict.SetValue{T}(string, T)"/>
+    /// </summary>
     public void SetVariable<T>(string name, T value)
     {
         data.SetValue(name, value);
     }
 
+    /// <summary>
+    /// Calls <see cref="VariableDict.GetObservableValue{T}(string)"/>
+    /// </summary>
     public IObservable<T> GetObservableVariable<T>(string name)
     {
         return data.GetObservableValue<T>(name);
     }
 
+    /// <summary>
+    /// Calls <see cref="VariableDict.GetVariables"/>
+    /// </summary>
     public List<string> GetVariables() { 
         return data.GetVariables();
     } 
--- Assets/Scripts/Model/VariableDict.cs
+++ Assets/Scripts/Model/VariableDict.cs
@@ -3,17 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UniRx;
 
+/// <summary>
+/// A data structure which mimics a dictionary that maps name to a value of any type
+/// </summary>
 public class VariableDict {
 
+    /// <summary>
+    /// A set containing the names of variables contained within the dictionary
+    /// </summary>
     HashSet<string> variableNames;
+
+    /// <summary>
+    /// A dictionary contains dictionaries for each different <see cref="Type"/>
+    /// </summary>
     Dictionary<Type, IDictionary> dictionaries;
 
+    /// <summary>
+    /// Create an empty VariableDict
+    /// </summary>
     public VariableDict()
     {
         variableNames = new HashSet<string>();
         dictionaries = new Dictionary<System.Type, IDictionary>();
     }
 
+    /// <summary>
+    ///     Get the dictionary associated with a type.
+    ///     Creates the dictionary if it does not exist already.
+    /// </summary>
+    /// <typeparam name="T">The type associated with the dictionary</typeparam>
+    /// <returns>The true dictioary mapping name -> Observable Type</returns>
     private Dictionary<string, BehaviorSubject<T>> GetDictionary<T>()
     {
         if (!dictionaries.ContainsKey(typeof(T)))
@@ -25,20 +44,44 @@ public class VariableDict {
         return (Dictionary<string, BehaviorSubject<T>>) dictionaries[type];
     }
 
+    /// <summary>
+    /// Get the current value of a variable.
+    /// </summary>
+    /// <typeparam name="T">The type of the variable</typeparam>
+    /// <param name="name">The name of the variable</param>
+    /// <returns>The value of the variable with name, or the default value for the type</returns>
     public T GetValue<T>(string name)
     {
+        // TODO: Should this throw an exception if the variable doesn't exist?
         if (Has<T>(name)) return GetDictionary<T>()[name].Value;
         return default(T);
     }
 
+    /// <summary>
+    ///     Get the observable value of a variable.
+    ///     Will create a new observable if the variable has not been set yet.
+    /// </summary>
+    /// <typeparam name="T">The type of the variable</typeparam>
+    /// <param name="name">The name of the variable</param>
+    /// <returns>An observable of the value of the variable.</returns>
     public IObservable<T> GetObservableValue<T>(string name)
     {
+        // TODO: Should this throw an exception if the variable doesn't exist?
         if (!Has<T>(name))
             SetValue(name, default(T));
 
         return GetDictionary<T>()[name];
     }
 
+    /// <summary>
+    ///     Set the value of the variable of type T.
+    /// </summary>
+    /// <typeparam name="T">The type of the variable</typeparam>
+    /// <param name="name">The name of the value</param>
+    /// <param name="value">The new value of the variable</param>
+    /// <exception cref="ArgumentException">
+    ///     If <typeparamref name="T"/> does not match the existing <see cref="Type"/> of the variable named <see cref="name"/>.
+    /// </exception>
     public void SetValue<T>(string name, T value)
     {
         if (Has<T>(name))
@@ -57,6 +100,12 @@ public class VariableDict {
         }
     }
 
+    /// <summary>
+    /// Check if a variable named <paramref name="name"/> of type <typeparamref name="T"/> exists
+    /// </summary>
+    /// <typeparam name="T">The type of the variable</typeparam>
+    /// <param name="name">The name of the variable</param>
+    /// <returns>True if the variable exists</returns>
     public bool Has<T>(string name)
     {
         return GetDictionary<T>().ContainsKey(name);
