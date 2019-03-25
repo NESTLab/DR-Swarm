c6b81daa0f4f3199f1460795725908baa57a0b5d
Jerry Brown <jbbrown@wpi.edu>
2019-03-25 16:56:41 -0400

    Add comments to ArgosClient.

--- Assets/Scripts/Networking/ArgosClient.cs
+++ Assets/Scripts/Networking/ArgosClient.cs
@@ -8,10 +8,34 @@ using System.Net.Sockets;
 using System.Threading;
 using FullSerializer;
 
+/// <summary>
+/// Connects to a Variable Publisher server and feeds the data into the data model
+/// </summary>
 public class ArgosClient : MonoBehaviour
 {
+    /// <summary>
+    /// The ip address or hostname of the server
+    /// </summary>
     [SerializeField]
-    public string serverAddr = "192.168.1.108";
+    private string _serverAddr = "192.168.1.108";
+    public string serverAddr
+    {
+        get { return _serverAddr; }
+        set
+        {
+            // Stop collecting data and wait for the thread to end.
+            collectData = false;
+            if (recvThread.Join(1000)) // Wait 1 second for it to die
+            {
+                recvThread.Abort(); // Thread didn't stop, kill it
+            }
+
+            // Change the server address and re-start the thread
+            _serverAddr = value;
+            collectData = true;
+            this.CreateRecvThread();
+        }
+    }
 
     private static readonly fsSerializer _serializer = new fsSerializer();
 
@@ -22,13 +46,26 @@ public class ArgosClient : MonoBehaviour
     private static int port = 8052;
 
     // Start is called before the first frame update
-    void Start()
+    void Start() { }
+
+    /// <summary>
+    /// Create a new thread to recieve data
+    /// </summary>
+    private void CreateRecvThread()
     {
         recvThread = new Thread(new ThreadStart(RecieveData));
         recvThread.IsBackground = true;
         recvThread.Start();
     }
 
+    /// <summary>
+    ///     Connects to the server at <see cref="serverAddr"/> and parse data from
+    ///     the sever into our data model.
+    /// </summary>
+    /// <remarks>
+    ///     Designed to run in a separate thread, since it has a while loop that
+    ///     runs for a long time.
+    /// </remarks>
     private void RecieveData()
     {
         client = new TcpClient();
@@ -36,10 +73,14 @@ public class ArgosClient : MonoBehaviour
         StringBuilder builder = new StringBuilder();
         NetworkStream stream = null;
         Byte[] data = new Byte[256];
+
+        // Loop until we're supposed to stop collecitng data
         while (collectData)
         {
             try
             {
+                // If we aren't connected try to connect
+                // (or re-connect if connection was lost at some point)
                 if (!client.Connected)
                 {
                     client.Connect(serverAddr, port);
@@ -50,11 +91,14 @@ public class ArgosClient : MonoBehaviour
 
                     if (bytes > 0)
                     {
-                        responseData = Encoding.ASCII.GetString(data, 0, bytes);
+                        // Decode the data from the server with UTF8 formatting
+                        responseData = Encoding.UTF8.GetString(data, 0, bytes);
 
+                        // Add the parsed data to the string builder and check if \n was recieved
                         builder.Append(responseData);
                         if (responseData.Contains("\n"))
                         {
+                            // Parse the data recieved as JSON
                             String jsonString = builder.ToString();
                             jsonString = jsonString.Substring(4);
                             Debug.Log(jsonString);
@@ -64,6 +108,8 @@ public class ArgosClient : MonoBehaviour
 
                             object dataPack = null;
                             _serializer.TryDeserialize(jsonData, typeof(SerializedDataPack), ref dataPack).AssertSuccessWithoutWarnings();
+
+                            // Assign the JSON data to the data model
                             ((SerializedDataPack)dataPack).AssignVariables();
                         }
                     }
@@ -76,6 +122,7 @@ public class ArgosClient : MonoBehaviour
             }
             catch (SocketException e)
             {
+                // If we disconnected, wait a second and loop again
                 Thread.Sleep(1000);
                 Debug.LogWarning(String.Format("Failed to connect to ARGoS server ({0}), re-attempting connection...", e.ToString()));
             }
@@ -85,17 +132,16 @@ public class ArgosClient : MonoBehaviour
             }
         }
 
+        // If client was created, try and close it
         if (client != null)
             client.Close();
 
+        // Kill thread
         recvThread.Abort();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
     private void OnDestroy()
     {
