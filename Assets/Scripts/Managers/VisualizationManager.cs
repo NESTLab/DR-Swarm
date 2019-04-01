b81188d5a3de82dbc289ff800c2c7be54f35049a
Jerry Brown <jbbrown@wpi.edu>
2019-03-31 22:10:19 -0400

    Move managers into folder

---
+++ Assets/Scripts/Managers/VisualizationManager.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using DrSwarm.Model;
+using DrSwarm.Model.Visualizations;
+
+namespace DrSwarm
+{
+    public class VisualizationManager : MonoBehaviour
+    {
+        // TODO: See if there is a better data structure for this class, this works for now though
+
+        // Dictionary that associates a robot with an observable set of visualizations
+        Dictionary<Robot, BehaviorSubject<HashSet<string>>> robotVisualizations; // TODO: find a better name for this
+
+        // Dictionary to associate a visualization's unique name with a observable IVisualization object
+        Dictionary<string, BehaviorSubject<IVisualization>> visualizations;
+
+        private enum UpdateKind
+        {
+            Add,
+            Remove
+        }
+
+        public VisualizationManager()
+        {
+            visualizations = new Dictionary<string, BehaviorSubject<IVisualization>>();
+            robotVisualizations = new Dictionary<Robot, BehaviorSubject<HashSet<string>>>();
+        }
+
+        #region SINGLETON PATTERN
+        // Thanks https://answers.unity.com/questions/891380/unity-c-singleton.html
+        private static VisualizationManager _instance;
+        public static VisualizationManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = GameObject.FindObjectOfType<VisualizationManager>();
+
+                    if (_instance == null)
+                    {
+                        GameObject container = new GameObject("VisualizationManager");
+                        _instance = container.AddComponent<VisualizationManager>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+        #endregion
+
+        #region Add/Edit/Remove Visualizations
+        public void AddVisualization(string name, IVisualization visualization)
+        {
+
+            // Add the visualization to the dictionary unless it's already been added
+            if (!visualizations.ContainsKey(name))
+            {
+                visualizations.Add(name, new BehaviorSubject<IVisualization>(visualization));
+                UpdateRobotVisualizations(name, visualization, UpdateKind.Add);
+            }
+            else
+            {
+                throw new Exception(String.Format("Cannot add visualization ('{0}'), already exists.", name));
+            }
+        }
+
+        public void EditVisualization(string name, IVisualization visualization)
+        {
+            BehaviorSubject<IVisualization> observable;
+            if (visualizations.TryGetValue(name, out observable))
+            {
+                // Compute the set of robots that were added and the set that were removed by the edit
+                ISet<Robot> addedRobots = new HashSet<Robot>(visualization.GetRobots());
+                addedRobots.ExceptWith(observable.Value.GetRobots());
+
+                ISet<Robot> removedRobots = new HashSet<Robot>(observable.Value.GetRobots());
+                removedRobots.ExceptWith(visualization.GetRobots());
+
+                // Update the observable set of visualizations for each added/removed robot
+                foreach (Robot robot in addedRobots)
+                {
+                    BehaviorSubject<HashSet<string>> visualizationNamesObs = GetRobotVisualizationSubject(robot);
+                    HashSet<string> nameSet = visualizationNamesObs.Value;
+                    nameSet.Add(name);
+                    visualizationNamesObs.OnNext(nameSet);
+                }
+
+                foreach (Robot robot in removedRobots)
+                {
+                    BehaviorSubject<HashSet<string>> visualizationNamesObs = GetRobotVisualizationSubject(robot);
+                    HashSet<string> nameSet = visualizationNamesObs.Value;
+                    nameSet.Remove(name);
+                    visualizationNamesObs.OnNext(nameSet);
+                }
+
+                // Update the visualization subject
+                observable.OnNext(visualization);
+            }
+            else
+            {
+                throw new Exception(String.Format("Cannot edit visualization ('{0}'), does not exist.", name));
+            }
+        }
+
+        public void RemoveVisualization(string name)
+        {
+            // Check if the visualization exists in the dictionary
+            BehaviorSubject<IVisualization> observable;
+            if (visualizations.TryGetValue(name, out observable))
+            {
+                // Send the complete event
+                observable.OnCompleted();
+
+                // Remove the visualization from all the robots it is associated with
+                UpdateRobotVisualizations(name, observable.Value, UpdateKind.Remove);
+
+                // Finally remove it from the dictionary
+                visualizations.Remove(name);
+            }
+            else
+            {
+                throw new Exception(String.Format("Cannot remove visualization ('{0}'), does not exist.", name));
+            }
+        }
+        #endregion
+
+        #region Observable Visualization List
+        private void UpdateRobotVisualizations(string name, IVisualization visualization, UpdateKind type)
+        {
+            // (Add/remove) the visualization for each robot it is associated with
+            foreach (Robot robot in visualization.GetRobots())
+            {
+                BehaviorSubject<HashSet<string>> visualizationNamesObs = GetRobotVisualizationSubject(robot);
+                HashSet<string> nameSet = visualizationNamesObs.Value;
+                switch (type)
+                {
+                    case UpdateKind.Add:
+                        nameSet.Add(name);
+                        break;
+
+                    case UpdateKind.Remove:
+                        nameSet.Remove(name);
+                        break;
+                }
+
+                // Update the observable set of visualizations associated with this robot
+                visualizationNamesObs.OnNext(nameSet);
+            }
+        }
+
+        private BehaviorSubject<HashSet<string>> GetRobotVisualizationSubject(Robot robot)
+        {
+            // Get the set of visualization names associated with this robot
+            BehaviorSubject<HashSet<string>> visualizationNamesObs;
+            if (robotVisualizations.TryGetValue(robot, out visualizationNamesObs))
+            {
+                return visualizationNamesObs;
+            }
+            else
+            {
+                // If it doesn't exist already, create a new observable set and return it
+                visualizationNamesObs = new BehaviorSubject<HashSet<string>>(new HashSet<string>());
+                robotVisualizations.Add(robot, visualizationNamesObs);
+                return visualizationNamesObs;
+            }
+        }
+
+        public IObservable<ISet<string>> GetObservableVisualizationsForRobot(Robot robot)
+        {
+            // Turn the subject of visualizations associated with this robot into an observable
+            // This makes it so classes other than VisualizationManager can only subscribe to this set
+            // instead of being able to OnNext, OnError, and OnComplete
+            return GetRobotVisualizationSubject(robot).AsObservable();
+        }
+        #endregion
+
+        #region Observable Visualizations
+        public Type GetVisualizationType(string name)
+        {
+            if (visualizations.ContainsKey(name))
+            {
+                return visualizations[name].Value.GetType();
+            }
+            else
+            {
+                throw new Exception(String.Format("Cannot get visualization type ('{0}'), does not exist", name));
+            }
+        }
+
+        public IObservable<IVisualization> GetObservableVisualization(string name)
+        {
+            if (visualizations.ContainsKey(name))
+            {
+                return visualizations[name].AsObservable();
+            }
+            else
+            {
+                throw new Exception(String.Format("Cannot get visualization ('{0}'), does not exist", name));
+            }
+        }
+        #endregion
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/VisualizationWindow.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+using DrSwarm.Model;
+using DrSwarm.Model.Visualizations;
+using DrSwarm.VisualizationContainers;
+
+namespace DrSwarm
+{
+    public class VisualizationWindow : MonoBehaviour
+    {
+        // The robot associated with this visualization window
+        private Robot robot;
+
+        // The canvas object of the visualization window, created from a prefab
+        private GameObject canvas;
+
+        // The transform of the container, which is a subobject ofthe canvas
+        private RectTransform container;
+
+        // A set of names for the visualization containers associated with this window
+        private HashSet<string> visualizationNames;
+
+        // Use this for initialization
+        void Start()
+        {
+            canvas = (GameObject)Instantiate(Resources.Load("VisualizationCanvas"), transform);
+            container = canvas.transform.Find("Container").GetComponent<RectTransform>();
+            visualizationNames = new HashSet<string>();
+            robot = DataManager.Instance.GetRobot(name);
+
+            // Subscribe to the set of visualizations for this robot
+            // A callback gets called if a visualization was added or removed for the robot
+            VisualizationManager.Instance.GetObservableVisualizationsForRobot(robot).Subscribe(VisualizationListUpdated);
+        }
+
+        private void VisualizationListUpdated(ISet<string> visualizationSet)
+        {
+            // Compute the set of visualizations added and removed
+            HashSet<string> addedVisualizations = new HashSet<string>(visualizationSet);
+            addedVisualizations.ExceptWith(visualizationNames);
+
+            HashSet<string> removedVisualizations = new HashSet<string>(visualizationNames);
+            removedVisualizations.ExceptWith(visualizationSet);
+
+            // Create and destory containers for added and removed visualizations
+            foreach (string visualizationName in removedVisualizations)
+            {
+                Destroy(container.transform.Find(visualizationName).gameObject);
+                visualizationNames.Remove(visualizationName);
+            }
+
+            foreach (string visualizationName in addedVisualizations)
+            {
+                CreateVisualizationContainer(visualizationName);
+                visualizationNames.Add(visualizationName);
+            }
+        }
+
+        private void CreateVisualizationContainer(string visualizationName)
+        {
+            // Create an empty 2D gameobject for the container to be stored in
+            GameObject gameObject = new GameObject(visualizationName, typeof(Image));
+            gameObject.transform.SetParent(container);
+            gameObject.GetComponent<Image>().color = Color.clear;
+
+            // Set the origin of the container to be in the bottom left
+            RectTransform transform = gameObject.GetComponent<RectTransform>();
+            transform.sizeDelta = container.GetComponent<RectTransform>().sizeDelta;
+            transform.anchorMin = Vector2.zero;
+            transform.anchorMax = Vector2.zero;
+            transform.pivot = Vector2.zero;
+            transform.localPosition = Vector3.zero;
+            transform.localScale = Vector3.one;
+            transform.localRotation = new Quaternion(0, 0, 0, 0);
+
+            // Create a container based off of the visualization type
+            // TODO: Try using "visitor" pattern here
+            Type visualizationType = VisualizationManager.Instance.GetVisualizationType(visualizationName);
+            if (visualizationType == typeof(LineGraph))
+            {
+                LineGraphContainer container = gameObject.AddComponent<LineGraphContainer>();
+                container.robot = this.robot;
+                container.visualizationName = visualizationName;
+                container.container = transform;
+            }
+            else if (visualizationType == typeof(PieChart))
+            {
+                PieChartContainer container = gameObject.AddComponent<PieChartContainer>();
+                container.robot = this.robot;
+                container.visualizationName = visualizationName;
+                container.container = transform;
+            }
+            else if (visualizationType == typeof(PieChartMultiVar))
+            {
+                PieChartMultiVarContainer container = gameObject.AddComponent<PieChartMultiVarContainer>();
+                container.robot = this.robot;
+                container.visualizationName = visualizationName;
+                container.container = transform;
+            }
+            else if (visualizationType == typeof(RangeIndicator))
+            {
+                RangeIndicatorContainer container = gameObject.AddComponent<RangeIndicatorContainer>();
+                container.robot = this.robot;
+                container.visualizationName = visualizationName;
+                container.container = transform;
+            }
+            else if (visualizationType == typeof(MapIndicator))
+            {
+                MapIndicatorContainer container = gameObject.AddComponent<MapIndicatorContainer>();
+                container.robot = this.robot;
+                container.visualizationName = visualizationName;
+                container.container = transform;
+            }
+            else if (visualizationType == typeof(BarGraph))
+            {
+                BarGraphContainer container = gameObject.AddComponent<BarGraphContainer>();
+                container.robot = this.robot;
+                container.visualizationName = visualizationName;
+                container.container = transform;
+            }
+            else
+            {
+                throw new Exception("Invalid visualization type.");
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            canvas.transform.LookAt(GameObject.Find("Camera").transform);
+
+            // Rotate 180 around Y axis, because LookAt points the Z axis at the camera
+            // when instead we want it pointing away from the camera
+            canvas.transform.Rotate(new Vector3(0, 180, 0), Space.Self);
+        }
+    }
+}
